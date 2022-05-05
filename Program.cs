e2f1194ee5b36f651c65a54bc4ab8c49e2a26b95
erdincDonmez <erdinc_donmez@yahoo.com>
2022-05-05 21:21:05 +0300

    firs commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Merhaba!");
+Console.WriteLine("=========");
+Console.Write("Adınız   : ");
+string ad = Console.ReadLine();
+Console.Write("Soyadınız: ");
+string soyad = Console.ReadLine();
+Console.WriteLine("Merhaba "+ad+" "+soyad);
+Console.ReadKey();
+
+
