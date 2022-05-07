07fe5a38db83630560dceaf869c538d6d73e74e1
erdincDonmez <erdinc_donmez@yahoo.com>
2022-05-08 00:35:06 +0300

    master to main

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Merhaba!");
+Console.WriteLine("=========");
+Console.Write("Adınız   : ");
+string ad = Console.ReadLine();
+Console.Write("Soyadınız: ");
+string soyad = Console.ReadLine();
+Console.WriteLine("Merhaba "+ad+" "+soyad);
+Console.ReadKey();
+*/
---
+++ Operatorler.cs
@@ -0,0 +1,20 @@
+// See https://aka.ms/new-console-template for more information
+// Deneme
+var a = 10;
+int b = 2;
+a /= 2;
+bool c = true;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+Console.ReadLine();
+
+/*
+Atama ve İşlemli Atama Operatörleri =, +=, -=, *=, /=
+Mantıksal Operatörler               ||, &&, !
+İlişkisel Operatörler               ==,!=, <, >, >=,<=
+Aritmetik                           +, -, *, /, %, ++, --
+*/
+
